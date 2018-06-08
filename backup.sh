a7d890dd041642a809a623535147763f364576af
Eugene Obrezkov <ghaiklor@gmail.com>
2018-06-08 22:23:52 +0300

    Add possibility to download repo as tar.gz

--- backup.sh
+++ backup.sh
@@ -23,55 +23,66 @@ function blue_color() {
     echo -e "${BLUE_COLOR}\\c"
 }
 
-function hello() {
-    blue_color
-    echo "                                              "
-    echo "               Backup My GitHub               "
-    echo "                 by @ghaiklor                 "
-    echo "                                              "
-    echo "This script will clone all your repositories from provided username to your machine"
-    echo "It will prompt you for your username account and personal access token"
-    echo "To generate token, please, refer this guide - https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line"
-    echo "Make sure, that your token has full access to repo scope!"
-    reset_color
-}
+### ----- ###
+### Hello ###
+### ----- ###
+blue_color
+echo "                                              "
+echo "               Backup My GitHub               "
+echo "                 by @ghaiklor                 "
+echo "                                              "
+echo "This script will download all your repositories from provided username to your machine"
+echo "It will prompt you for your username account and personal access token"
+echo "To generate token, please, refer this guide - https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line"
+echo "Make sure, that your token has full access to repo scope!"
+reset_color
 
-function checkForCurl() {
-    if ! [ "$(command -v curl)" ]; then
-        red_color
-        echo "You don't have installed curl"
-        exit 1
-    else
-        green_color
-        echo "curl is present on your machine, continue..."
-    fi
-
-    reset_color
-}
-
-function checkForJQ() {
-    if ! [ "$(command -v jq)" ]; then
-        red_color
-        echo "You don't have installed jq"
-        exit 1
-    else
-        green_color
-        echo "jq is present on your machine, continue..."
-    fi
-
-    reset_color
-}
-
-function cloneRepositories() {
+### -------------- ###
+### Check for curl ###
+### -------------- ###
+if ! [ "$(command -v curl)" ]; then
+    red_color
+    echo "You don't have installed curl"
+    exit 1
+else
     green_color
-    echo
-    read -r -p "What is your username on GitHub: " username
-    read -r -p "What is your personal access token: " token
-    echo
+    echo "curl is present on your machine, continue..."
+fi
+reset_color
 
+### ------------ ###
+### Check for jq ###
+### ------------ ###
+if ! [ "$(command -v jq)" ]; then
+    red_color
+    echo "You don't have installed jq"
+    exit 1
+else
+    green_color
+    echo "jq is present on your machine, continue..."
+fi
+reset_color
+
+### ---------------------- ###
+### Prompt for credentials ###
+### ---------------------- ###
+green_color
+echo
+read -r -p "What is your username on GitHub: " GITHUB_USERNAME
+read -r -p "What is your personal access token: " GITHUB_TOKEN
+echo
+reset_color
+
+### ------------------ ###
+### Clone Repositories ###
+### ------------------ ###
+green_color
+read -r -n 1 -p "Do you want to clone repositories [Y/n]: " answer
+echo
+if [ "${answer}" != "n" ]; then
     blue_color
-    repository_count=$(curl -XGET -s https://"${username}":"${token}"@api.github.com/users/"${username}" | jq -c --raw-output ".public_repos")
-    repositories=$(curl -XGET -s https://"${username}":"${token}"@api.github.com/users/"${username}"/repos?per_page="${repository_count}" | jq -c --raw-output ".[].ssh_url")
+    repository_count=$(curl -XGET -s https://"${GITHUB_USERNAME}":"${GITHUB_TOKEN}"@api.github.com/users/"${GITHUB_USERNAME}" | jq -c --raw-output ".public_repos")
+    repositories=$(curl -XGET -s https://"${GITHUB_USERNAME}":"${GITHUB_TOKEN}"@api.github.com/users/"${GITHUB_USERNAME}"/repos?per_page="${repository_count}" | jq -c --raw-output ".[].ssh_url")
 
     for repository in ${repositories}; do
         echo "Cloning ${repository}..."
@@ -79,10 +90,36 @@ function cloneRepositories() {
     done
 
     green_color
-    echo "All your repositories are successfully cloned in current directory"
-}
+    echo "All your ${repository_count} repositories are successfully cloned in current directory"
+    echo
+    reset_color
+fi
 
-hello
-checkForCurl
-checkForJQ
-cloneRepositories
+### ---------------- ###
+### Download Archive ###
+### ---------------- ###
+green_color
+read -r -n 1 -p "Do you want to download your repositories as an archive (master branch) [Y/n]: " answer
+echo
+if [ "${answer}" != "n" ]; then
+    blue_color
+    repository_count=$(curl -XGET -s https://"${GITHUB_USERNAME}":"${GITHUB_TOKEN}"@api.github.com/users/"${GITHUB_USERNAME}" | jq -c --raw-output ".public_repos")
+    repositories=$(curl -XGET -s https://"${GITHUB_USERNAME}":"${GITHUB_TOKEN}"@api.github.com/users/"${GITHUB_USERNAME}"/repos?per_page="${repository_count}" | jq -c --raw-output ".[].name")
+
+    for repository in ${repositories}; do
+        echo "Downloading ${repository}.tar.gz..."
+        curl -L -s -o ${repository}.tar.gz https://github.com/${GITHUB_USERNAME}/${repository}/archive/master.tar.gz
+    done
+
+    green_color
+    echo "All your ${repository_count} repositories are successfully downloaded in current directory"
+    echo
+    reset_color
+fi
+
+### ------ ###
+### Footer ###
+### ------ ###
+green_color
+echo "Everything is done, thanks for having me :)"
+reset_color
